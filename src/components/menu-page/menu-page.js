d52561f2470f69fff243234257c0a2292c0b895e
Magmusacy <xpfk.098@gmail.com>
2023-09-04 00:38:19 +0200

    Create menu page

---
+++ src/components/menu-page/menu-page.js
@@ -0,0 +1,53 @@
+import './menu-page.css';
+import PierogiPizza from './pierogi-pizza.jpg';
+import CheesePizza from './cheese-pizza.jpg';
+import ArugulaPizza from './arugula-pizza.jpg';
+import SalamiPizza from './salami-pizza.jpg';
+
+function createDish(name, priceInt, imageSrc, ingredients) {
+    const dishCard = document.createElement('div');
+    dishCard.classList.add('dish-card')
+    
+    const dishName = document.createElement('h2');
+    dishName.innerText = name;
+    
+    const dishPrice = document.createElement('span');
+    dishPrice.innerText = `€$${priceInt}`;
+    
+    const dishCardHeading = document.createElement('div');
+    dishCardHeading.classList.add('card-heading');
+    dishCardHeading.appendChild(dishName);
+    dishCardHeading.appendChild(dishPrice);
+    dishCard.appendChild(dishCardHeading);
+
+    const dishPicture = new Image();
+    dishPicture.src = imageSrc;
+    dishPicture.alt = name;
+    dishCard.appendChild(dishPicture);
+
+    const descriptionHeading = document.createElement('h3');
+    descriptionHeading.innerText = 'Ingredients:'
+    dishCard.appendChild(descriptionHeading);
+
+    const dishDescription = document.createElement('p');
+    dishDescription.innerText = ingredients;
+    dishCard.appendChild(dishDescription);
+
+    return dishCard;
+}
+
+function menuPage() {
+    const content = document.querySelector('#content');
+    const dish1 = createDish('Piergi Pizza', 5, PierogiPizza, 'Pizza dough, mashed potatoes, cheddar cheese, bacon bits, sautéed onions, sour cream, fresh chives, salt, pepper.');
+    const dish2 = createDish('Cheese Pizza', 7, CheesePizza, 'Pizza dough, tomato sauce, olive oil, dried oregano, dried basil, salt, pepper, mozzarella cheese, Parmesan cheese, fresh basil leaves.');
+    const dish3 = createDish('Arugula Pizza', 4, ArugulaPizza, 'Pizza dough, olive oil, garlic, mozzarella cheese, Parmesan cheese, fresh arugula, balsamic vinegar, salt, and pepper');
+    const dish4 = createDish('Salami Pizza', 9, SalamiPizza, 'Pizza dough, pizza sauce, mozzarella cheese, sliced salami, sliced red bell peppers, sliced red onions, olive oil, dried oregano, salt, and pepper');
+
+
+    content.appendChild(dish1);
+    content.appendChild(dish2);
+    content.appendChild(dish3);
+    content.appendChild(dish4);
+};
+
+export default menuPage();
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import aboutPage from './components/about-page/about-page.js';
+import menuPage from './components/menu-page/menu-page.js';
 import './normalize.css'
 import './style.css'
 
-aboutPage();
+menuPage();
+// aboutPage();
