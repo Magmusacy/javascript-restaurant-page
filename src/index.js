302720c0fcf88aa9fcba770a7cc44d6d475b6141
Magmusacy <xpfk.098@gmail.com>
2023-09-04 01:10:34 +0200

    Add ability to switch between pages

--- src/index.js
+++ src/index.js
@@ -3,5 +3,27 @@ import menuPage from './components/menu-page/menu-page.js';
 import './normalize.css'
 import './style.css'
 
-menuPage();
-// aboutPage();
+const aboutButton = document.querySelector('#about');
+const menuButton = document.querySelector('#menu');
+const contactButton = document.querySelector('#contact');
+const content = document.querySelector('#content');
+
+const restartState = () => {
+    content.innerHTML = '';
+    content.className = '';
+    for (let button of [aboutButton, menuButton, contactButton]) {
+        button.classList.remove('selected');
+    }
+};
+
+aboutButton.addEventListener('click', () => {
+    restartState();
+    aboutButton.classList.add('selected');
+    aboutPage();
+});
+
+menuButton.addEventListener('click', () => {
+    restartState();
+    menuButton.classList.add('selected');
+    menuPage();
+});
